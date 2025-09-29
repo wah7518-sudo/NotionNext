0172a3e8026d263689ffbc6382db8416d3c78e67
wah7518-sudo <wah7518@gmail.com>
2025-09-29 17:57:42 +0800

    Update SideRight.js

--- themes/hexo/components/SideRight.js
+++ themes/hexo/components/SideRight.js
@@ -1,10 +1,10 @@
-import Live2D from '@/components/Live2D'
+
 import { siteConfig } from '@/lib/config'
 import { useGlobal } from '@/lib/global'
 import dynamic from 'next/dynamic'
 import CONFIG from '../config'
 import { AnalyticsCard } from './AnalyticsCard'
-import Announcement from './Announcement'
+
 import Card from './Card'
 import Catalog from './Catalog'
 import CategoryGroup from './CategoryGroup'
@@ -93,14 +93,14 @@ export default function SideRight(props) {
             </Card>
           )}
 
-        <Announcement post={notice} />
+     
 
         {siteConfig('COMMENT_WALINE_SERVER_URL') &&
           siteConfig('COMMENT_WALINE_RECENT') && <HexoRecentComments />}
 
         {rightAreaSlot}
         <FaceBookPage />
-        <Live2D />
+
       </div>
     </div>
   )
